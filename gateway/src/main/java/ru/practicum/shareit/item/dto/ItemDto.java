7cc285953688bd19695f4c5f90723e9cda43c28b
vzhemaitis <vyacheslavzhemaitis@yandex.com>
2023-02-08 18:06:49 +0300

    0.52 TZ16 all controllers and clients added to gateway UserDto validation moved to gateway

--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -3,13 +3,10 @@ package ru.practicum.shareit.item.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-/*import ru.practicum.shareit.booking.dto.BookingInfoDto;
-import ru.practicum.shareit.comment.dto.CommentDto;*/
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
@@ -28,82 +25,7 @@ public class ItemDto {
     private Boolean available;
     private Long owner;
     private Long requestId;
-   /* private BookingInfoDto lastBooking;
-    private BookingInfoDto nextBooking;
-    private List<CommentDto> comments;*/
 
     public ItemDto() {
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-
-    public Long getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Long owner) {
-        this.owner = owner;
-    }
-
-    public Long getRequestId() {
-        return requestId;
-    }
-
-    public void setRequestId(Long requestId) {
-        this.requestId = requestId;
-    }
-
-    /*public BookingInfoDto getLastBooking() {
-        return lastBooking;
-    }
-
-    public void setLastBooking(BookingInfoDto lastBooking) {
-        this.lastBooking = lastBooking;
-    }
-
-    public BookingInfoDto getNextBooking() {
-        return nextBooking;
-    }
-
-    public void setNextBooking(BookingInfoDto nextBooking) {
-        this.nextBooking = nextBooking;
-    }
-
-    public List<CommentDto> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<CommentDto> comments) {
-        this.comments = comments;
-    }*/
 }
